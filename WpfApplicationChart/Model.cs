3cec55d9e1ccb63a5732b26652135f00db7af2bb
ПП ПППП ПП <ledinhvu2806@niuitmo.ru>
2021-03-20 13:27:17 +0300

    Add project files.

---
+++ WpfApplicationChart/Model.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace WpfApplicationChart
+{
+
+    class Model
+    {
+        public IList<Data> Data
+        {
+            get
+            {
+                IList<Data> list = new List<Data>
+                    {
+                    new Data("Иванов", 10),
+                    new Data("Петров", 30),
+                    new Data("Сиборов", 10),
+                    new Data("Кузнецов", 20),
+                    new Data("Павлов", 40)
+                    };
+                return list;
+            }
+        }
+    }
+    public class Data
+    {
+        public Data(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+}
